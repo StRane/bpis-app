f264bb03763a3b618a8b49b324db9ca125170572
StRane <73998688+StRane@users.noreply.github.com>
2022-11-03 15:46:36 +0700

    mobile test

--- components/UI/gallery/gallerytext.tsx
+++ components/UI/gallery/gallerytext.tsx
@@ -13,19 +13,12 @@ interface Props {
 
 const GalleryContent = (props: Props) => {
 
-  const learnMore:string = 'Learn more'
-
-
-
-
   const transitionsTop = useTransition(props, {
     from: { opacity: 0, transform: "translateY(-100%)" },
     enter: { opacity: 1, transform: "scale(1)" },
     leave: { opacity: 0, transform: "scale(0.5)" },
     exitBeforeEnter: true,
   });
-
-
 
   const transitionsMid = useTransition(props, {
     from: { opacity: 0, transform: "translateX(100%)" },
@@ -38,21 +31,21 @@ const GalleryContent = (props: Props) => {
   });
 
   const transitionsButton = useTransition(props, {
-    from: { opacity: 0  },
-    enter: { opacity: 1, transform: "scale(1)", visibility:"visible"},
-    leave: { opacity: 0, transform: "scale(0.4)" ,visibility:"hidden"},
+    from: { opacity: 0 },
+    enter: { opacity: 1, transform: "scale(1)", visibility: "visible" },
+    leave: { opacity: 0, transform: "scale(0.4)", visibility: "hidden" },
     exitBeforeEnter: true,
   });
 
   const transitionsBack = useTransition(props, {
-    from: { opacity: 0, },
-    enter: { opacity: 1},
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
     leave: { opacity: 0 },
     exitBeforeEnter: true,
   });
 
   return props.main ? (
-    <div className="w-full h-full absolute top-0 grid grid-cols-9 text-center">
+    <div className="grid-span-12 h-full relative top-0 grid grid-cols-9 text-center">
       <GalleryButton
         clicked={props.clicked}
         style="col-span-1 h-10 w-10 md:h-20 md:w-20 self-center text-white text-center align-middle text-xl  rounded-full bg-bpisgry2/30 hover:bg-bpisg z-20 mx-auto"
@@ -65,85 +58,83 @@ const GalleryContent = (props: Props) => {
       />
     </div>
   ) : (
-    <>
-      <div className="w-screen h-full absolute top-0 grid grid-cols-9 text-center">
-        <GalleryButton
-          clicked={props.clicked}
-          style="col-span-1 h-10 w-10 md:h-20 md:w-20 self-center text-white text-center align-middle text-xl md:text-4xl rounded-full bg-bpisgry2/30 hover:bg-bpisg z-20 mx-auto"
-          type="decrease"
-        />
-        <div className="col-span-7 justify-center flex flex-col z-20 text-white">
-          {transitionsTop((animation, props) => {
-            return (
-              <a.h3
-                className="text-2xl md:text-5xl xl:text-7xl pb-16 font-semibold"
-                style={{
-                  ...animation,
-                }}
-              >
-                {props.toptext}
-                <>
-                  {props.schoolname ? (
-                    <span className="text-bpisg "> BPIS</span>
-                  ) : (
-                    <></>
-                  )}
-                </>
-              </a.h3>
-            );
-          })}
+    <div className="col-span-full min-w-[100vw] h-full relative top-0 grid grid-cols-9 text-center">
+      <GalleryButton
+        clicked={props.clicked}
+        style="col-span-1 h-10 w-10 md:h-20 md:w-20 self-center text-white text-center align-middle text-xl md:text-4xl rounded-full bg-bpisgry2/30 hover:bg-bpisg z-20 mx-auto"
+        type="decrease"
+      />
+      <div className="col-span-7 justify-center flex flex-col z-20 text-white">
+        {transitionsTop((animation, props) => {
+          return (
+            <a.h3
+              className="text-2xl md:text-5xl xl:text-7xl pb-16 font-semibold"
+              style={{
+                ...animation,
+              }}
+            >
+              {props.toptext}
+              <>
+                {props.schoolname ? (
+                  <span className="text-bpisg"> BPIS</span>
+                ) : (
+                  <></>
+                )}
+              </>
+            </a.h3>
+          );
+        })}
 
-          {transitionsMid((animation, props) => {
-            return (
-              <a.p
-                className="justify-center font-normal text-base md:text-lg xl:text-xl tracking-wider text-bpisgry2 pb-16"
-                style={{
-                  ...animation,
-                }}
-              >
-                {props.bottext}
-              </a.p>
-            );
-          })}
+        {transitionsMid((animation, props) => {
+          return (
+            <a.p
+              className="justify-center font-normal text-base md:text-lg xl:text-xl tracking-wider text-bpisgry2 pb-16"
+              style={{
+                ...animation,
+              }}
+            >
+              {props.bottext}
+            </a.p>
+          );
+        })}
 
-          {transitionsButton((animation) => {
-            return (
-              <a.button
-                className="hover:bg-bpisg w-1/2 md:w-1/5"
-                style={{
-                  visibility: "hidden",
-                  lineHeight: "2.5rem",
-                  textTransform: "uppercase",
-                  borderStyle: "solid",
-                  borderColor: "white",
-                  borderWidth: "1px",
-                  alignSelf: "center",
-                  borderRadius: "1.5rem",
-                  ...animation,
-                }}
-              >
-                {learnMore}
-              </a.button>
-            );
-          })}
-        </div>
-        <GalleryButton
-          clicked={props.clicked}
-          style="col-start-9 col-end-10 h-10 w-10 md:h-20 md:w-20 self-center text-white text-center align-middle text-xl md:text-4xl rounded-full bg-bpisgry2/30 hover:bg-bpisg z-20 mx-auto"
-          type="increase"
-        />
+        {transitionsButton((animation) => {
+          return (
+            <a.button
+              className="hover:bg-bpisg w-1/2 md:w-1/5"
+              style={{
+                visibility: "hidden",
+                lineHeight: "2.5rem",
+                textTransform: "uppercase",
+                borderStyle: "solid",
+                borderColor: "white",
+                borderWidth: "1px",
+                alignSelf: "center",
+                borderRadius: "1.5rem",
+                ...animation,
+              }}
+            >
+              Learn More
+            </a.button>
+          );
+        })}
       </div>
+      <GalleryButton
+        clicked={props.clicked}
+        style="col-start-9 col-end-10 h-10 w-10 md:h-20 md:w-20 self-center text-white text-center align-middle text-xl md:text-4xl rounded-full bg-bpisgry2/30 hover:bg-bpisg z-20 mx-auto"
+        type="increase"
+      />
       {transitionsBack((animation) => {
         return (
           <a.div
-            className="w-full h-full relative top-0 grid grid-cols-9 text-center bg-bpisshdwless"
+            className="w-full h-full absolute top-0 text-center bg-bpisshdwless"
             style={{
               ...animation,
             }}
           />
         );
       })}
-    </>
+    </div>
   );
 };
 
--- components/UI/gallery/maingallery.tsx
+++ components/UI/gallery/maingallery.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-
 import GalleryContent from "./gallerytext";
 
 import Image from "next/image";
+
 
 import { useTransition, animated as a } from "react-spring";
 
@@ -11,6 +11,7 @@ import slider1 from "../../../images/main_gallery/BPIS-logo-slider.webp";
 import slider2 from "../../../images/main_gallery/slider1.webp";
 import slider3 from "../../../images/main_gallery/slider2.webp";
 import slider4 from "../../../images/main_gallery/slider3.webp";
+import useWindowDimensions from "../../helper/WindowDimensions";
 
 type galleryItem = {
   name: string;
@@ -62,6 +63,9 @@ const MainGallery = () => {
     },
   ];
 
+  let window = useWindowDimensions();
+
+
   const clickHandler = (decider: number) => {
     let newItem = currentPhoto + decider;
     const max = photos.length - 1;
@@ -77,29 +81,40 @@ const MainGallery = () => {
   const currentPic: galleryItem = photos[currentPhoto];
 
   const transitions = useTransition(currentPic, {
-    from: { opacity: 0, transform: "scale(1.1)" },
-    enter: { opacity: 1, transform: "scale(1)" },
-    leave: { opacity: 0, transform: "scale(0.9)" },
+    from: {
+      opacity: 0,
+      transform: "scale(1.1)",
+    },
+    enter: {
+      opacity: 1,
+      transform: "scale(1)",
+    },
+    leave: {
+      opacity: 0,
+      transform: "scale(0.9)",
+    },
     exitBeforeEnter: true,
   });
 
   return (
-    <div className="relative w-screen h-[90vh] left-0 mt-20">
-      <div className="w-full h-full absolute top-0">
+    <div className="col-span-full min-w-full h-[90vh] left-0">
+      <div className="w-full h-full absolute top-0 left-0">
         {transitions((animation, props) => {
           return (
             <a.div
               key={props.id}
               style={{
-                position:"relative",
-                height:"100%",
+                position: "relative",
+                height: "90vh",
+                width: "screen",
                 ...animation,
               }}
             >
-              <img
+              <Image
                 src={props.pic}
                 alt={props.name}
-                className="object-cover h-full w-full"
+                fill={true}
+                className="object-cover h-[90vh] w-screen"
               />
             </a.div>
           );
